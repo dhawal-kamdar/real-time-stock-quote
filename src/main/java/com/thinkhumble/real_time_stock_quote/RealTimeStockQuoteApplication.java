13f221006fc71a41f3a5762eef93bc7656f4d8ab
dhawal-kamdar <dhawalkamdar7@gmail.com>
2025-07-02 16:41:25 +0530

    Initial commit

---
+++ src/main/java/com/thinkhumble/real_time_stock_quote/RealTimeStockQuoteApplication.java
@@ -0,0 +1,13 @@
+package com.thinkhumble.real_time_stock_quote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeStockQuoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeStockQuoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinkhumble/real_time_stock_quote/RealTimeStockQuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkhumble.real_time_stock_quote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeStockQuoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
