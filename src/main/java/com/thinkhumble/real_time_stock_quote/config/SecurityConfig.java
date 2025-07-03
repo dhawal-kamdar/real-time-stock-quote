499bfc21b44f3816fbd6758bb7e092563b8c6bf7
dhawal-kamdar <dhawalkamdar7@gmail.com>
2025-07-03 13:08:59 +0530

    Added Spring Security

---
+++ src/main/java/com/thinkhumble/real_time_stock_quote/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.thinkhumble.real_time_stock_quote.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
+
+}
--- src/main/java/com/thinkhumble/real_time_stock_quote/model/StockQuote.java
+++ src/main/java/com/thinkhumble/real_time_stock_quote/model/StockQuote.java
@@ -1,14 +1,14 @@
 package com.thinkhumble.real_time_stock_quote.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class StockQuote {
 
+    @JsonProperty("symbol")
     private String symbol;
     private double open;
     private double high;
@@ -19,5 +19,116 @@ public class StockQuote {
     private double previousClose;
     private double change;
     private String percentChange;
+
+    public StockQuote() {}
+
+    public StockQuote(String symbol, double open, double high, double low, double price, int volume, String latestTradingDay, double previousClose, double change, String percentChange) {
+        this.symbol = symbol;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.price = price;
+        this.volume = volume;
+        this.latestTradingDay = latestTradingDay;
+        this.previousClose = previousClose;
+        this.change = change;
+        this.percentChange = percentChange;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getLatestTradingDay() {
+        return latestTradingDay;
+    }
+
+    public void setLatestTradingDay(String latestTradingDay) {
+        this.latestTradingDay = latestTradingDay;
+    }
+
+    public double getPreviousClose() {
+        return previousClose;
+    }
+
+    public void setPreviousClose(double previousClose) {
+        this.previousClose = previousClose;
+    }
+
+    public double getChange() {
+        return change;
+    }
+
+    public void setChange(double change) {
+        this.change = change;
+    }
+
+    public String getPercentChange() {
+        return percentChange;
+    }
+
+    public void setPercentChange(String percentChange) {
+        this.percentChange = percentChange;
+    }
+
+    @Override
+    public String toString() {
+        return "StockQuote{" +
+                "symbol='" + symbol + '\'' +
+                ", open=" + open +
+                ", high=" + high +
+                ", low=" + low +
+                ", price=" + price +
+                ", volume=" + volume +
+                ", latestTradingDay='" + latestTradingDay + '\'' +
+                ", previousClose=" + previousClose +
+                ", change=" + change +
+                ", percentChange='" + percentChange + '\'' +
+                '}';
+    }
 
 }
